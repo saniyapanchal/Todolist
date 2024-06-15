b75547b397c92eefc6d81ca868618ab496be1465
saniyapanchal <154547669+saniyapanchal@users.noreply.github.com>
2024-06-15 17:44:16 +0530

    Add files via upload

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,33 @@
+import { Todo } from "../types/todo";
+import TodoItem from "./TodoItem"
+
+interface TodoListProps {
+    todos: Todo[];
+    onCompletedChange: (id: number, completed: boolean) => void;
+    onDelete: (id: number) => void;
+}
+
+export default function TodoList({
+todos,
+onCompletedChange,
+onDelete
+}: TodoListProps) {
+    const todoSorted = todos.sort((a,b) => {
+        if (a.completed === b.completed){
+            return b.id - a.id;
+        }
+        return a.completed ? 1 : -1;
+    })
+
+    return (
+        <div className="space-y-2">
+        {todoSorted.map(todo => (
+            <TodoItem
+            key={todo.id}
+            todo={todo} 
+            onCompletedChange={onCompletedChange}
+            onDelete={onDelete}/>
+        ))}
+        </div>
+    )
+}
